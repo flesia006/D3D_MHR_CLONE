8168d69cf9505e3e58ca10d81f40f761da7302f7
원영 최 <wyoung0502@gmail.com>
2024-02-14 23:03:59 +0900

    특납 자세 이후 칼집 위치 이상한거 수정

--- DirectX3D/Objects/Character/Player.cpp
+++ DirectX3D/Objects/Character/Player.cpp
@@ -685,6 +685,9 @@ void Player::Roll()
 		float rot = atan2(newForward.x, newForward.z);
 		Rot().y = rot;
 	}
+
+	holdingSword = false;
+
 	if (State_S())
 		SetState(S_017);
 	else if (!State_S())
@@ -1792,7 +1795,10 @@ void Player::L153() // 특수납도 취소 동작
 	PLAY;
 
 	if (RATIO > 0.98)
+	{
+		holdingSword = false;
 		SetState(S_001);
+	}
 }
 
 void Player::L154() // 앉아발도 베기
