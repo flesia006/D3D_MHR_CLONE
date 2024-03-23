70fd7c5860512fb9f362d39e9a43af05f2df304a
임성두 <dlatjden@naver.com>
2024-03-23 19:33:39 +0900

    ㅋ

--- DirectX3D/Objects/Character/Valphalk.cpp
+++ DirectX3D/Objects/Character/Valphalk.cpp
@@ -2853,21 +2853,20 @@ void Valphalk::S_JetRush()
 	{
 		if (!renderJet)
 			renderJet = true;
-		SetState(E_2013);	E2013();
+		SetState(E_2015);	E2015();
 	}
 
-	if (sequence == 3) // 돌진 브레이크 모션
+	if (sequence == 4) // 돌진 브레이크 모션
 	{
-
 		SetState(E_2017);	E2017();
 	}
 
-	if (sequence == 4) // 바닥에 착지 모션
+	if (sequence == 5) // 바닥에 착지 모션
 	{
 		SetState(E_2019);	E2019();
 	}
 
-	if (sequence == 5) // 마무리
+	if (sequence == 6) // 마무리
 	{
 		if (renderJet)
 			renderJet = false;
@@ -4253,7 +4252,8 @@ void Valphalk::E2015()//돌진중 Loop
 {
 	PLAY;
 	if (RATIO > 0.96)
-		SetState(E_2017);
+		sequence++;
+		//SetState(E_2017);
 }
 
 void Valphalk::E2017()//돌진 브레이크
